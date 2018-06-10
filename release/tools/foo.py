597e03059fb59328d8fb22a6b375972299ea609c
ont-tester <ont-tester@tester.com>
2018-06-10 10:23:46 +0800

    Signed-off-by: ont-tester <ont-tester@tester.com>

---
+++ release/tools/foo.py
@@ -0,0 +1,10 @@
+import base58
+
+
+print(base58.b58encode('02802683d48aa9b34f9a0e91f5e27fedc8ac6486b1'))
+
+print(base58.b58decode('StV1DL6CwTryKyV'))
+#base58.b58encode_check(b'hello world')
+
+#base58.b58decode_check(b'3vQB7B6MrGQZaxCuFg4oh')
+#base58.b58decode_check(b'4vQB7B6MrGQZaxCuFg4oh')
