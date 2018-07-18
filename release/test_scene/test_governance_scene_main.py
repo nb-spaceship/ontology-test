289cf290c413f82d8dedf158c9b767c0346200b8
FroyoZzz <35131402+FroyoZzz@users.noreply.github.com>
2018-07-18 16:39:08 +0800

    update

--- release/test_scene/test_governance_scene_main.py
+++ release/test_scene/test_governance_scene_main.py
@@ -19,13 +19,13 @@ from utils.config import Config
 from utils.taskdata import TaskData, Task
 from utils.logger import LoggerInstance
 from utils.hexstring import *
-from utils.api.commonapi import *
+from utils.commonapi import *
 from utils.error import Error
-from utils.api.init_ong_ont import *
+from utils.init_ong_ont import *
 from utils.parametrizedtestcase import ParametrizedTestCase
 from test_api import *
 from test_common import *
-from utils.api.rpcapi import *
+from utils.rpcapi import *
 
 logger = LoggerInstance
 rpcapiTest=RPCApi()
@@ -49,25 +49,26 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		
 	def test_01(self):
 		logger.open("test01.log", "test01")
-		(result, response) = getGlobalParam()
+		# (result, response) = getGlobalParam()
 		(result, response) = invoke_function_update("updateGlobalParam",param0=500000000000,param1=10000,param2=49,param3=20,param4=50,param5=50,param6=5,param7=5)
-		(result, response) = getGlobalParam()
+		# (result, response) = getGlobalParam()
 		logger.close(result)
 		
 	def test_02(self):
 		logger.open("test02.log", "test02")
-		(result, response) = getGlobalParam()
+		# (result, response) = getGlobalParam()
 		(result, response) = invoke_function_update("updateGlobalParam",param0=500000000000,param1=10000,param2=49,param3=20,param4=50,param5=50,param6=5,param7=5)
-		(result, response) =  getGlobalParam()
+		# (result, response) =  getGlobalParam()
 		logger.close(result)
 
 	def test_03(self):
 		logger.open("test03.log", "test03")
-		(result, response) = getGlobalParam()
-		(result, response) = getGovernanceView()
-		(result, response) = invoke_function_SplitCurve() ##
-		(result, response) = getPeerPoolItem()   ### test() 
-		(result, response) = getTotalStake()
+		# (result, response) = getGlobalParam()
+		# (result, response) = getGovernanceView()
+		# (result, response) = invoke_function_SplitCurve() 
+		##存在但是invoke_function_SplitCurve(array)中的array不清楚
+		# (result, response) = getPeerPoolItem()   ### test() 
+		# (result, response) = getTotalStake()
 		init(candidate=True, register_ontid=True, restart=True, pub_key="1", ont_count="10000")
 		time.sleep(5)
 		for num in range(7,14):
@@ -91,11 +92,11 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		
 	def test_04(self):
 		logger.open("test04.log", "test04")
-		(result, response) = getGlobalParam()
-		(result, response) = getGovernanceView()
-		(result, response) = getSplitCurve()
-		(result, response) = getPeerPoolItem()  
-		(result, response) = getTotalStake()
+		# (result, response) = getGlobalParam()
+		# (result, response) = getGovernanceView()
+		# (result, response) = invoke_function_SplitCurve()
+		# (result, response) = getPeerPoolItem()  
+		# (result, response) = getTotalStake()
 		init(candidate=True, register_ontid=True, restart=True, pub_key="1", ont_count="10000")
 		time.sleep(5)
 		for num in range(7,14):
@@ -117,11 +118,11 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		
 	def test_05(self):
 		logger.open("test05.log", "test05")
-		(result, response) = getGlobalParam()
-		(result, response) = getGovernanceView()
-		(result, response) = getSplitCurve()
-		(result, response) = getPeerPoolItem()  
-		(result, response) = getTotalStake()
+		# (result, response) = getGlobalParam()
+		# (result, response) = getGovernanceView()
+		# (result, response) = invoke_function_SplitCurve()
+		# (result, response) = getPeerPoolItem()  
+		# (result, response) = getTotalStake()
 		init(candidate=True, register_ontid=True, restart=True, pub_key="1", ont_count="10000")
 		time.sleep(5)
 		for num in range(7,14):
@@ -143,11 +144,11 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		
 	def test_06(self):
 		logger.open("test06.log", "test06")
-		(result, response) = getGlobalParam()
-		(result, response) = getGovernanceView()
-		(result, response) = getSplitCurve()
-		(result, response) = getPeerPoolItem()  
-		(result, response) = getTotalStake()
+		# (result, response) = getGlobalParam()
+		# (result, response) = getGovernanceView()
+		# (result, response) = invoke_function_SplitCurve()
+		# (result, response) = getPeerPoolItem()  
+		# (result, response) = getTotalStake()
 		init(candidate=True, register_ontid=True, restart=True, pub_key="1", ont_count="10000")
 		time.sleep(5)
 		for num in range(7,14):
@@ -227,7 +228,7 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		
 		(result, response) = rpcapiTest.getbalance(Config.NODES[9]["address"])
 		time.sleep(5)
-		(result, response) = GetTotalStake()
+		# (result, response) = GetTotalStake()
 		time.sleep(5)
 		logger.close(result)	
 		
@@ -244,7 +245,7 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		time.sleep(5)
 		(result, response) = nodeCountCheck(response,8)			
 		time.sleep(5)
-		(result, response) = getPeerPoolItem()
+		# (result, response) = getPeerPoolItem()
 		time.sleep(5)
 		rpcapiTest.getbalance(Config.NODES[10]["address"])
 		time.sleep(5)
@@ -252,13 +253,13 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		for n in range(0,7):
 			(result, response) = invoke_function_vote("voteForPeer",Config.NODES[n]["address"],voteList=[Config.NODES[10]["pubkey"]],voteCount,errorcode = 0,node_index =10) 
 			time.sleep(5)		
-		(result, response) = getPeerPoolItem()
+		# (result, response) = getPeerPoolItem()
 		time.sleep(5)
 		(result, response) = rpcapiTest.getbalance(Config.NODES[10]["address"])
 		time.sleep(5)
-		(result, response) = GetTotalStake()
+		# (result, response) = GetTotalStake()
 		time.sleep(5)
-		(result, response) = getVoteInfo()
+		# (result, response) = getVoteInfo()
 		time.sleep(5)
 		logger.close(result)
 		
@@ -280,11 +281,11 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		for n in range(0,7):
 			(result, response) = invoke_function_vote("unVoteForPeer",Config.NODES[n]["address"],voteList=[Config.NODES[11]["pubkey"]],voteCount,errorcode = 0,node_index =11) 
 			time.sleep(5)		
-		(result, response) = getPeerPoolItem()
+		# (result, response) = getPeerPoolItem()
 		time.sleep(5)
 		(result, response) = rpcapiTest.getbalance(Config.NODES[11]["address"])
 		time.sleep(5)
-		(result, response) = GetTotalStake()
+		# (result, response) = GetTotalStake()
 		time.sleep(5)
 		logger.close(result)
 		
@@ -297,21 +298,21 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		(result, response) = invoke_function_candidate("approveCandidate",Config.NODES[12]["pubkey"],errorcode=0)
 		time.sleep(5)	
 		(result, response) = nodeCountCheck(response,8)
-		(result, response) = GetPeerPoolItem() #chakan node2 status  02
+		# (result, response) = GetPeerPoolItem() #chakan node2 status  02
 		time.sleep(5)
-		(result, response) = GetVoteInfo()
+		# (result, response) = GetVoteInfo()
 		time.sleep(5)
 		(result, response) = invoke_function_quitNode("quitNode",Config.NODES[1]["pubkey"],Config.NODES[1]["address"],1,0)
 		time.sleep(5)	
-		(result, response) = GetPeerPoolItem() #chakan node2 status  03
+		# (result, response) = GetPeerPoolItem() #chakan node2 status  03
 		time.sleep(5)		
 		(result, response) = invoke_function_commitDpos(0)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() 
+		# (result, response) = GetPeerPoolItem() 
 		time.sleep(5)
 		(result, response) = invoke_function_commitDpos(0)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem()   #nodequit(yiding)
+		# (result, response) = GetPeerPoolItem()   #nodequit(yiding)
 		time.sleep(5)
 		(result, response) = nodeCountCheck(response,7)
 		time.sleep(5)
@@ -327,21 +328,21 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		(result, response) = invoke_function_candidate("approveCandidate",Config.NODES[13]["pubkey"],errorcode=0)
 		time.sleep(5)	
 		(result, response) = nodeCountCheck(response,8)
-		(result, response) = GetPeerPoolItem() #chakan node14 status  01
+		# (result, response) = GetPeerPoolItem() #chakan node14 status  01
 		time.sleep(5)
-		(result, response) = GetVoteInfo()
+		# (result, response) = GetVoteInfo()
 		time.sleep(5)
 		(result, response) = invoke_function_quitNode("quitNode",Config.NODES[1]["pubkey"],Config.NODES[1]["address"],1,0)
 		time.sleep(5)	
-		(result, response) = GetPeerPoolItem() #chakan node2 status  04
+		# (result, response) = GetPeerPoolItem() #chakan node2 status  04
 		time.sleep(5)		
 		(result, response) = invoke_function_commitDpos(0)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() 
+		# (result, response) = GetPeerPoolItem() 
 		time.sleep(5)
 		(result, response) = invoke_function_commitDpos(0)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem()   #nodequit(yiding)
+		# (result, response) = GetPeerPoolItem()   #nodequit(yiding)
 		time.sleep(5)
 		(result, response) = nodeCountCheck(response,7)
 		logger.close(result)
@@ -359,19 +360,13 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		time.sleep(5)
 		(result, response) = nodeCountCheck(response,14)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
 		for n in range(0,7):
 			(result, response) = invoke_function_vote("voteForPeer",Config.NODES[n]["address"],voteList=[Config.NODES[12]["pubkey"]],voteCount,errorcode = 0,node_index =12) 
 			time.sleep(5)
-		
-		time.sleep(20)
-		time.sleep(20)
-		# (result, response) = invoke_function_commitDpos(0)
-		# time.sleep(5)
-		# (result, response) = invoke_function_commitDpos(0)
-		# time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		time.sleep(1000)
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
 		logger.close(result)
 	
@@ -388,7 +383,7 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		time.sleep(5)
 		(result, response) = nodeCountCheck(response,14)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
 		for n in range(0,7):
 			(result, response) = invoke_function_vote("voteForPeer",Config.NODES[n]["address"],voteList=[Config.NODES[12]["pubkey"]],voteCount,errorcode = 0,node_index =12) 
@@ -397,7 +392,7 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		time.sleep(5)
 		(result, response) = invoke_function_commitDpos(0)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
 		logger.close(result)
 		
@@ -414,16 +409,10 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		time.sleep(5)
 		(result, response) = nodeCountCheck(response,14)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
-		
-		time.sleep(20)
-		time.sleep(20)
-		# (result, response) = invoke_function_commitDpos(0)
-		# time.sleep(5)
-		# (result, response) = invoke_function_commitDpos(0)
-		# time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		time.sleep(1000)
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
 		logger.close(result)
 		
@@ -440,13 +429,13 @@ class test_zhiliChangjing(ParametrizedTestCase):
 		time.sleep(5)
 		(result, response) = nodeCountCheck(response,14)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
 		(result, response) = invoke_function_commitDpos(0)
 		time.sleep(5)
 		(result, response) = invoke_function_commitDpos(0)
 		time.sleep(5)
-		(result, response) = GetPeerPoolItem() #chakan node13 status 01
+		# (result, response) = GetPeerPoolItem() #chakan node13 status 01
 		time.sleep(5)
 		logger.close(result)
 	
