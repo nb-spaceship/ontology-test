c5d776d2b3793402e9720d18067c96b39a47c7b4
FroyoZzz <35131402+FroyoZzz@users.noreply.github.com>
2018-07-18 15:30:18 +0800

    update

--- release/install.sh
+++ release/install.sh
@@ -4,7 +4,7 @@ pip3 install requests
 pip3 install websocket-client
 pip3 install base58
 pip3 install ddt
-pip3 install paramiko
+pip3 install paramiko --user
 
 chmod 777 tools/deploy_contract.sh
 chmod 777 run_all.sh
