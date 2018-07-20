9fd80397b352af59a9b1ffd584eb447a9668aed6
ont-tester <ont-tester@tester.com>
2018-07-20 18:12:19 +0800

    Signed-off-by: ont-tester <ont-tester@tester.com>

--- test_tool/monitor/monitor.py
+++ test_tool/monitor/monitor.py
@@ -2,13 +2,22 @@ import time
 import os
 import unittest
 
+sys.path.append('..')
+sys.path.append('../..')
+
+from utils.logger import LoggerInstance as logger 
+
 TRY_RECOVER_TIMES = 3
 
 class TestMonitor:
 	#分析环境，未知错误的检查
 	def __init__(self):
-		self.faild_count = 0;
-		self.total_count = 0;
+		self.faild_count = 0
+		self.total_count = 0
+
+	def reset(self):
+		self.faild_count = 0
+		self.total_count = 0
 
 	def analysis_log(self, logpath):
 		f = open(logpath, 'r')
@@ -30,10 +39,6 @@ class TestMonitor:
 
 		return False
 
-	#特定的环境检查
-	def check_env(self):
-		return False
-
 	#恢复测试环境
 	def recover_env(self):
 		return False
@@ -42,6 +47,9 @@ class TestMonitor:
 		runner = unittest.TextTestRunner()
 		for testsuit in testsuits:
 			runner.run(testsuit)
+
+			self.reset()
+			self.analysis_env(logger.prefix + "/" + )
 
 if __name__ == '__main__':
 	TestMonitor().analysis_env("../")
