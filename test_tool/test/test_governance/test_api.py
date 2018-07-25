6139dd4937c09f6caa18a7d10764f74c0b25bd39
FroyoZzz <35131402+FroyoZzz@users.noreply.github.com>
2018-07-25 18:40:26 +0800

    update

---
+++ test_tool/test/test_governance/test_api.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from test_config import test_config
+
+def get_config():
+    wallet_A_address   = test_config.wallet_A_address
+    wallet_B_address   = test_config.wallet_B_address
+    vote_price 		   = test_config.vote_price
+    node_B_puiblic_key = test_config.node_B_puiblic_key
+    blocks_per_round   = test_config.blocks_per_round
+    punish_ratio       = test_config.punish_ratio
+    return (wallet_A_address, wallet_B_address, vote_price, node_B_puiblic_key, blocks_per_round, punish_ratio)
