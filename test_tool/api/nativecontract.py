9cb2c194bbc5169320195ec62aa51d3079363f89
ont-tester <ont-tester@tester.com>
2018-07-26 12:46:20 +0800

    c

--- test_tool/api/nativecontract.py
+++ test_tool/api/nativecontract.py
@@ -30,7 +30,7 @@ class NativeApi:
     ##############################################
     ###0100000000000000000000000000000000000000###
     @staticmethod
-    def allowance_ont(neo_contract_address, from_address, to_address, amount, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def allowance_ont(neo_contract_address, from_address, to_address, amount, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
                     "REQUEST":  {
                     "Qid": "t",
@@ -51,7 +51,7 @@ class NativeApi:
             }
         if node_index != None:
             request["NODE_INDEX"] = node_index
-        return CONTRACT_API.call_contract(Task(name="allowance_ont", ijson=request), twice = True) 
+        return CONTRACT_API.call_contract(Task(name="allowance_ont", ijson=request), twice = True, sleep=sleep) 
 
     def transfer_ont(self, pay_address, get_address, amount, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
@@ -80,11 +80,11 @@ class NativeApi:
         if node_index != None:
             request["NODE_INDEX"] = node_index
 
-        return CONTRACT_API.call_contract(Task(name="transfer", ijson=request), twice=True, sleep=5)
+        return CONTRACT_API.call_contract(Task(name="transfer", ijson=request), twice=True, sleep=sleep)
 
     ##############################################
     ###0200000000000000000000000000000000000000###
-    def allowance_ong(self, neo_contract_address, from_address, to_address, amount, node_index=None,errorcode=0,gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def allowance_ong(self, neo_contract_address, from_address, to_address, amount, node_index=None,errorcode=0,gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
                     "REQUEST":  {
                     "Qid": "t",
@@ -106,7 +106,7 @@ class NativeApi:
         if node_index != None:
             request["NODE_INDEX"] = node_index
 
-        return CONTRACT_API.call_contract(Task(name="allowance_ong", ijson=request), twice = True) 
+        return CONTRACT_API.call_contract(Task(name="allowance_ong", ijson=request), twice = True, sleep=sleep) 
 
     def transfer_ong(self, pay_address, get_address, amount, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
@@ -135,11 +135,11 @@ class NativeApi:
         if node_index != None:
             request["NODE_INDEX"] = node_index
 
-        return CONTRACT_API.call_contract(Task(name="transfer", ijson=request), twice=True, sleep=5)
+        return CONTRACT_API.call_contract(Task(name="transfer", ijson=request), twice=True, sleep=sleep)
 
     ##############################################
     ###0300000000000000000000000000000000000000###
-    def regid_with_publickey(self, node_index, errorcode = 0):
+    def regid_with_publickey(self, node_index, errorcode = 0, sleep=5):
         ontid = Config.NODES[int(node_index)]["ontid"]
         pubkey = Config.NODES[int(node_index)]["pubkey"]
         request = {
@@ -162,11 +162,11 @@ class NativeApi:
         }
         
         request["NODE_INDEX"] = node_index
-        return CONTRACT_API.call_contract(Task(name ="regIDWithPublicKey", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name ="regIDWithPublicKey", ijson=request), twice = True, sleep=sleep)
 
     ##############################################
     ###0600000000000000000000000000000000000000###
-    def bind_role_function(self, contract_address, admin_address, role_str, functions, public_key="1", node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def bind_role_function(self, contract_address, admin_address, role_str, functions, public_key="1", node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -194,9 +194,9 @@ class NativeApi:
         else:
             node_index = Config.ontid_map[admin_address]
             request["NODE_INDEX"] = node_index
-        return CONTRACT_API.call_contract(Task(name="bind_role_function", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="bind_role_function", ijson=request), twice = True, sleep=sleep)
 
-    def bind_user_role(self, contract_address, admin_address, role_str, ontIDs, public_key="1", node_index = None, error_code = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def bind_user_role(self, contract_address, admin_address, role_str, ontIDs, public_key="1", node_index = None, error_code = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -225,9 +225,9 @@ class NativeApi:
             node_index = Config.ontid_map[admin_address]
             request["NODE_INDEX"] = node_index
             
-        return CONTRACT_API.call_contract(Task(name="bind_user_role", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="bind_user_role", ijson=request), twice = True, sleep=sleep)
 
-    def delegate_user_role(self, contract_address, owner_user, delegate_user, delegate_role, period, level, public_key="1", node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def delegate_user_role(self, contract_address, owner_user, delegate_user, delegate_role, period, level, public_key="1", node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -258,10 +258,10 @@ class NativeApi:
             node_index = Config.ontid_map[owner_user]
             request["NODE_INDEX"] = node_index
 
-        return CONTRACT_API.call_contract(Task(name="delegate_user_role", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="delegate_user_role", ijson=request), twice = True, sleep=sleep)
 
 
-    def withdraw_user_role(self, contract_address, call_user, delegate_user, delegate_role, public_key="1", node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def withdraw_user_role(self, contract_address, call_user, delegate_user, delegate_role, public_key="1", node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -290,11 +290,11 @@ class NativeApi:
             node_index = Config.ontid_map[call_user]
             request["NODE_INDEX"] = node_index
             
-        return CONTRACT_API.call_contract(Task(name="withdraw_user_role", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="withdraw_user_role", ijson=request), twice = True, sleep=sleep)
 
     ##############################################
     ###0700000000000000000000000000000000000000###
-    def vote_for_peer(self, walletAddress,voteList,voteCount,node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def vote_for_peer(self, walletAddress,voteList,voteCount,node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -322,9 +322,9 @@ class NativeApi:
                     request["NODE_INDEX"] = Config.NODES.index(node)
                     break
             
-        return CONTRACT_API.call_contract(Task(name="invoke_function_vote", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="invoke_function_vote", ijson=request), twice = True, sleep=sleep)
 
-    def unvote_for_peer(self, wallet_address, nodes_to_vote, ballot_to_vote, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def unvote_for_peer(self, wallet_address, nodes_to_vote, ballot_to_vote, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -352,9 +352,9 @@ class NativeApi:
                     request["NODE_INDEX"] = Config.NODES.index(node)
                     break
 
-        return CONTRACT_API.call_contract(Task(name="invoke_function_unvote", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="invoke_function_unvote", ijson=request), twice = True, sleep=sleep)
         
-    def withdraw_ont(self, wallet_address, nodes_to_vote, ballot_to_vote, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def withdraw_ont(self, wallet_address, nodes_to_vote, ballot_to_vote, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -383,9 +383,9 @@ class NativeApi:
                     request["NODE_INDEX"] = Config.NODES.index(node)
                     break
 
-        return CONTRACT_API.call_contract(Task(name="invoke_function_withdraw", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="invoke_function_withdraw", ijson=request), twice = True, sleep=sleep)
     
-    def update_global_param(self, param0,param1,param2,param3,param4,param5,param6,param7,errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def update_global_param(self, param0,param1,param2,param3,param4,param5,param6,param7,errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -411,9 +411,9 @@ class NativeApi:
             "RESPONSE":{"error" : errorcode}
         }
 
-        return CONTRACT_API.call_multisig_contract(Task(name="invoke_function_updateGlobalParam", ijson=request),Config.AdminNum,Config.AdminPublicKeyList)
+        return CONTRACT_API.call_multisig_contract(Task(name="invoke_function_updateGlobalParam", ijson=request),Config.AdminNum,Config.AdminPublicKeyList, sleep=sleep)
 
-    def quit_node(self, node_public_key, wallet_address, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def quit_node(self, node_public_key, wallet_address, node_index=None, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -440,9 +440,9 @@ class NativeApi:
                     request["NODE_INDEX"] = Config.NODES.index(node)
                     break
 
-        return CONTRACT_API.call_contract(Task(name="invoke_function_quitNode", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="invoke_function_quitNode", ijson=request), twice = True, sleep=sleep)
 
-    def black_node(self, node_public_key, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def black_node(self, node_public_key, errorcode=0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -461,7 +461,7 @@ class NativeApi:
             "RESPONSE": {"error" : errorcode}
         }
 
-        return CONTRACT_API.call_multisig_contract(Task(name="invoke_function_blackNode", ijson=request),Config.AdminNum,Config.AdminPublicKeyList)
+        return CONTRACT_API.call_multisig_contract(Task(name="invoke_function_blackNode", ijson=request),Config.AdminNum,Config.AdminPublicKeyList, sleep=sleep)
 
 
     def commit_dpos(self, errorcode = 0, gas_price = Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
@@ -483,10 +483,10 @@ class NativeApi:
             "RESPONSE":{"error" : errorcode}
         }
 
-        return CONTRACT_API.call_multisig_contract(Task(name="commit_dpos", ijson=request),Config.AdminNum,Config.AdminPublicKeyList)
+        return CONTRACT_API.call_multisig_contract(Task(name="commit_dpos", ijson=request),Config.AdminNum,Config.AdminPublicKeyList, sleep=sleep)
 
     #same to invoke_function_approve
-    def approve_candidate(self, pubKey, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def approve_candidate(self, pubKey, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "NODE_INDEX":0,
             "REQUEST": {
@@ -506,10 +506,10 @@ class NativeApi:
             "RESPONSE":{"error" : errorcode}
         }
             
-        return CONTRACT_API.call_multisig_contract(Task(name="approve_candidate", ijson=request),Config.AdminNum,Config.AdminPublicKeyList)
+        return CONTRACT_API.call_multisig_contract(Task(name="approve_candidate", ijson=request),Config.AdminNum,Config.AdminPublicKeyList, sleep=sleep)
 
     #same to invoke_function_register
-    def register_candidate(self, pubKey, walletAddress, ontCount, ontID, user, node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def register_candidate(self, pubKey, walletAddress, ontCount, ontID, user, node_index = None, errorcode = 0, gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -538,11 +538,11 @@ class NativeApi:
             node_index = Config.ontid_map[ontID]
             request["NODE_INDEX"] = node_index
         
-        return CONTRACT_API.call_contract(Task(name="invoke_function_register", ijson=request), twice = True)
+        return CONTRACT_API.call_contract(Task(name="invoke_function_register", ijson=request), twice = True, sleep=sleep)
 
 
 ##############################################
-    def transferFrom_multi(self, put_address, amount, node_index = None,errorcode=0,public_key_Array=[], errorkey = "error", gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def transferFrom_multi(self, put_address, amount, node_index = None,errorcode=0,public_key_Array=[], errorkey = "error", gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -568,9 +568,9 @@ class NativeApi:
             request["SIGN_RESPONSE"]={errorkey : errorcode}
 
         request["NODE_INDEX"] = node_index    
-        return CONTRACT_API.call_multisig_contract(Task(name="transferFrom_multi", ijson=request),public_key_Array[0],public_key_Array[1])
+        return CONTRACT_API.call_multisig_contract(Task(name="transferFrom_multi", ijson=request),public_key_Array[0],public_key_Array[1], sleep=sleep)
         
-    def transfer_multi(self, assetStr,put_address, get_address,amount, node_index = None,errorcode=0,public_key_Array=[], errorkey = "error", gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT):
+    def transfer_multi(self, assetStr,put_address, get_address,amount, node_index = None,errorcode=0,public_key_Array=[], errorkey = "error", gas_price= Config.DEFAULT_GAS_PRICE, gas_limit = Config.DEFAULT_GAS_LIMIT, sleep=5):
         request = {
             "REQUEST": {
                 "Qid": "t",
@@ -590,24 +590,24 @@ class NativeApi:
             request["SIGN_RESPONSE"]={errorkey : errorcode}
 
         request["NODE_INDEX"] = node_index    
-        return CONTRACT_API.call_multisig_contract(Task(name="transfer_multi", ijson=request),public_key_Array[0],public_key_Array[1])
+        return CONTRACT_API.call_multisig_contract(Task(name="transfer_multi", ijson=request),public_key_Array[0],public_key_Array[1], sleep=sleep)
 
-    def init_ont_ong(self, node_count = 7):
+    def init_ont_ong(self, node_count = 7, sleep=sleep):
         for i in range(node_count):
-            (result, response)=self.transfer_multi("ont",Config.MULTI_SIGNED_ADDRESS,Config.NODES[i]["address"],10000000,public_key_Array=[5,[Config.NODES[0]["pubkey"],Config.NODES[1]["pubkey"],Config.NODES[2]["pubkey"],Config.NODES[3]["pubkey"],Config.NODES[4]["pubkey"],Config.NODES[5]["pubkey"],Config.NODES[6]["pubkey"]]])
+            (result, response)=self.transfer_multi("ont",Config.MULTI_SIGNED_ADDRESS,Config.NODES[i]["address"],10000000,public_key_Array=[5,[Config.NODES[0]["pubkey"],Config.NODES[1]["pubkey"],Config.NODES[2]["pubkey"],Config.NODES[3]["pubkey"],Config.NODES[4]["pubkey"],Config.NODES[5]["pubkey"],Config.NODES[6]["pubkey"]]], sleep=sleep)
             if not result:
                 return (result, response)
         if not NODE_API.wait_gen_block():
             return (False, "wait_gen_block time out[1]")
 
-        (result, response) = self.transferFrom_multi(Config.MULTI_SIGNED_ADDRESS,Config.INIT_AMOUNT_ONG,5,public_key_Array=[5,[Config.NODES[0]["pubkey"],Config.NODES[1]["pubkey"],Config.NODES[2]["pubkey"],Config.NODES[3]["pubkey"],Config.NODES[4]["pubkey"],Config.NODES[5]["pubkey"],Config.NODES[6]["pubkey"]]])       
+        (result, response) = self.transferFrom_multi(Config.MULTI_SIGNED_ADDRESS,Config.INIT_AMOUNT_ONG,5,public_key_Array=[5,[Config.NODES[0]["pubkey"],Config.NODES[1]["pubkey"],Config.NODES[2]["pubkey"],Config.NODES[3]["pubkey"],Config.NODES[4]["pubkey"],Config.NODES[5]["pubkey"],Config.NODES[6]["pubkey"]]], sleep=sleep)       
         if not result:
             return (result, response)
         if not NODE_API.wait_gen_block():
             return (False, "wait_gen_block time out[2]")
 
         for i in range(node_count):
-            (result, response)=self.transfer_multi("ong",Config.MULTI_SIGNED_ADDRESS,Config.NODES[i]["address"], int(int(Config.INIT_AMOUNT_ONG) / node_count),public_key_Array=[5,[Config.NODES[0]["pubkey"],Config.NODES[1]["pubkey"],Config.NODES[2]["pubkey"],Config.NODES[3]["pubkey"],Config.NODES[4]["pubkey"],Config.NODES[5]["pubkey"],Config.NODES[6]["pubkey"]]])
+            (result, response)=self.transfer_multi("ong",Config.MULTI_SIGNED_ADDRESS,Config.NODES[i]["address"], int(int(Config.INIT_AMOUNT_ONG) / node_count),public_key_Array=[5,[Config.NODES[0]["pubkey"],Config.NODES[1]["pubkey"],Config.NODES[2]["pubkey"],Config.NODES[3]["pubkey"],Config.NODES[4]["pubkey"],Config.NODES[5]["pubkey"],Config.NODES[6]["pubkey"]]], sleep=sleep)
             if not result:
                 return (result, response)
         if not NODE_API.wait_gen_block():
