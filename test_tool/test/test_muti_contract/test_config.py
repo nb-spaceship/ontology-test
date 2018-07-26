5bc9c4d6656d3040da33551566ec2ecb112e02c4
ont-tester <ont-tester@tester.com>
2018-07-26 10:36:25 +0800

    c

---
+++ test_tool/test/test_muti_contract/test_config.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+import ddt
+import unittest
+import urllib
+import urllib.request
+import json
+import os
+import sys, getopt
+import time
+
+sys.path.append('..')
+sys.path.append('../..')
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from utils.config import Config
+from utils.taskdata import TaskData, Task
+from utils.logger import LoggerInstance as logger
+from utils.hexstring import *
+from utils.error import Error
+from utils.common import Common
+
+class test_config():
+    testpath = os.path.dirname(os.path.realpath(__file__))
+    
+
+        
+
