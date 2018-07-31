c8185560f5b0cf4c348f1177bcaf212d68e8d831
bigchao <499089820@qq.com>
2018-07-31 21:44:58 +0800

    c

--- test_tool/test/test_benefit_model/test_main.py
+++ test_tool/test/test_benefit_model/test_main.py
@@ -619,6 +619,7 @@ class test_benefit_model_2(ParametrizedTestCase):
  
 			API.native().update_global_param("0", "1000", "32", "10", "50","50", "5", "5")
 			
+			API.node().start_nodes([13], Config.DEFAULT_NODE_ARGS, True, True)
 			(process, response) = API.native().transfer_ont(Config.NODES[0]["address"], Config.NODES[vote_node]["address"], "5000000", gas_price = 0)
 			(process, response) = API.native().transfer_ont(Config.NODES[0]["address"], Config.NODES[vote_node]["address"], "1000", gas_price = 0)
 
