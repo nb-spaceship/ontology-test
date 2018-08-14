94e8811925b84897744b34324e0d9e78a9a931b8
EnglishTing <wxqqywt@163.com>
2018-08-14 11:19:00 +0800

    update

--- sdk-test/src/test/resources/com/ontio/sdkapi/rest.cs
+++ sdk-test/src/test/resources/com/ontio/sdkapi/rest.cs
@@ -1,26 +1,38 @@
-using Ont.SmartContract.Framework.Services.Ont;
 using Ont.SmartContract.Framework;
+using Ont.SmartContract.Framework.Services.Ont;
+using Ont.SmartContract.Framework.Services.System;
 using System;
 using System.ComponentModel;
+using System.Numerics;
+
 
 namespace Ont.SmartContract
 {
-    public class HelloWorld : Framework.SmartContract
+    public class rpcapiTest : Framework.SmartContract
     {
         public static object Main(string operation, params object[] args)
         {
+            
             switch (operation)
-            {
-                case "Hello":
-                    Hello((string)args[0]);
-                    return true;
-                default:
+            {	
+            	case "test":
+	                byte[] key = (byte[]) args[0];
+	                byte[] value = (byte[]) args[1];
+	                PutStorge(Storage.CurrentContext, key, value);
+	                return GetStorge(Storage.CurrentContext, key);
+	            default:
                     return false;
             }
         }
-        public static void Hello(string msg)
+
+        public static byte[] GetStorge(StorageContext context, byte[] key)
         {
-            Runtime.Notify(msg);
+            return Storage.Get(context, key);
+        }
+
+        public static void PutStorge(StorageContext context, byte[] key, byte[] value)
+        {
+            Storage.Put(context, key, value);
         }
     }
 }
