7e55b155e902d804f38c63421f2eebfbf7c4b51d
FroyoZzz <35131402+FroyoZzz@users.noreply.github.com>
2018-08-16 15:00:28 +0800

    update
    run all test

--- sdk-test/src/test/java/com/ontio/RunAllTest.java
+++ sdk-test/src/test/java/com/ontio/RunAllTest.java
@@ -2,18 +2,14 @@ package com.ontio;
 
 import org.junit.runner.Request;
 import org.junit.runner.Result;
-import org.junit.runner.manipulation.Filter;
-import org.junit.runner.notification.RunNotifier;
-import org.apache.commons.lang.ArrayUtils;
+import org.junit.runner.notification.Failure;
 import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -31,7 +27,13 @@ import com.ontio.sdkapi.ONG_Native;
 import com.ontio.sdkapi.ONT_Native;
 import com.ontio.sdkapi.Ontid;
 import com.ontio.sdkapi.RPC_API;
+import com.ontio.sdkapi.Restful_API;
+import com.ontio.sdkapi.WebSocket_API;
 import com.ontio.testtool.OntTest;
+import com.ontio.testtool.utils.Config;
+
+import edu.emory.mathcs.backport.java.util.Collections;
+
 import com.ontio.TestMonitor;
 
 public class RunAllTest {
@@ -58,6 +60,7 @@ public class RunAllTest {
         
         // prarameter_t = "base";  
         // prarameter_f = "ClaimRecord.test_abnormal_002_exportIdentityQRCode";  
+        
         // prarameter_c = "C:\\Users\\tpc\\Desktop\\a.json";
         // prarameter_e = "Sample.test_base_001_Sample1";
         
@@ -97,7 +100,7 @@ public class RunAllTest {
         	JSONObject _json = OntTest.common().loadJson(prarameter_c);
         	Set<String> keys = _json.keySet();
         	for (String _key : keys) {
-        		if (_json.getString(_key).equals(true)) {
+        		if (_json.getString(_key).equals("true")) {
         			_files.add(_key);
         		}
         	}
@@ -115,12 +118,32 @@ public class RunAllTest {
         all_class.add(ONT_Native.class);
         all_class.add(RPC_API.class);
         all_class.add(Ontid.class);
-
+        all_class.add(Restful_API.class);
+        all_class.add(WebSocket_API.class);
+		
         JUnitCore junitRunner = new JUnitCore();
         junitRunner.addListener(new TestMonitor());
+        
+        double faultTolerance = 1;
+        
         for (Class<?> _class : all_class) {
+        	int total_cases = 0;
+            int failed_cases = 0;
+            Result result = null;
+        	
+        	// filter sheets
+        	if (!_files.isEmpty() && !_files.contains(_class.getSimpleName())){
+        		continue;
+        	}
+        	
 	        Request request = Request.aClass(_class);
 	        request = request.filterWith(new MethodNameFilter(_files, _types, _methods,_excludes));
+	        
+	        // if request is empty, continue
+	        if (request.getRunner().getClass().getMethods()[0].getDeclaringClass().toString().toLowerCase().contains("error")) {
+	        	continue;
+	        }
+	        
 	        request = request.sortWith(new Comparator<Description>(){
 				@Override
 				public int compare(Description d1, Description d2) {
@@ -137,7 +160,7 @@ public class RunAllTest {
 		        long costtime = 0;
 		        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		        
-		        Result result = junitRunner.run(request);
+		        result = junitRunner.run(request);
 		        
 	            endtime = System.currentTimeMillis();
 	            costtime = endtime - startTime;
@@ -156,128 +179,46 @@ public class RunAllTest {
 				e.printStackTrace();
 			}
 	        
-	        if (!TestMonitor.failedDescription.isEmpty()) {
-	        	for (Description failedDescription : TestMonitor.failedDescription){
-	        		Request retryRequest = Request.method(failedDescription.getClass(), failedDescription.getMethodName());
-	        		Result result = junitRunner.run(retryRequest);
+	        total_cases = result.getRunCount();
+	        System.out.println(result.getRunCount());
+	        List<Failure> failedCases = result.getFailures();
+	        
+	        failed_cases = result.getFailureCount();
+	        System.out.println(result.getFailureCount());
+	        
+	        if (!TestMonitor.blockDescription.isEmpty()) {
+	        	List<Description> tmpBlockDescription = new ArrayList<Description>();
+	        	Collections.copy(TestMonitor.blockDescription, tmpBlockDescription); ;
+	        	
+	        	for (Description blockDescription : tmpBlockDescription){
+	        		Request retryRequest = Request.method(blockDescription.getTestClass(), blockDescription.getMethodName());
+	        		result = junitRunner.run(retryRequest);
+	        		if (!result.wasSuccessful()) {
+	        			OntTest.logger().setBlock();
+	        		}
 	        		System.out.println(result.wasSuccessful());
 	        	}
 	        }
-        }
-        /*
-        Method[] all_methods = null;
-        for (Class<?> testClass : all_class) {
-        	all_methods = (Method[]) ArrayUtils.addAll(all_methods, testClass.getMethods());
-        }
-        
-        
-        Method[] tmpmethods = new Method[all_methods.length];
-        
-        JUnitCore junitRunner = new JUnitCore();
-        int i = 0;
-        
-        for (Method method : all_methods) {
-        	if (method.isAnnotationPresent(org.junit.Test.class)) {
-	        	// System.out.println(_types.toString());
+	        
+	        System.out.println(failed_cases / (double)total_cases);
+	        
+	        if (failed_cases / (double)total_cases > faultTolerance) {
+	        	// recover
+	    		OntTest.api().node().restart(new int[]{0,1,2,3,4,5,6}, "ontology", "config.json", Config.DEFAULT_NODE_ARGS);
 	        	
-	        	if (method.getName().equals("test_init")) {
-	        		tmpmethods[i++] = method;
-	        		continue;
-	        	}
-	        	
-	        	if (!_excludes.isEmpty() && _excludes.contains(method.getName())){
-	        		continue;
-	        	}
-	        	
-	        	String[] typeLen = method.getDeclaringClass().getTypeName().split("\\.");
-	        	String m_file = typeLen[typeLen.length-1].toString();
-	        	
-	        	System.out.println(method.getName().split("_")[1].toString());
-	        	
-	        	if (_methods.isEmpty() && (_types.isEmpty() || _types.contains(method.getName().split("_")[1].toString()))) {
-	        		if (_files.isEmpty() || _files.contains(m_file)) {
-	        			tmpmethods[i++] = method;
-		        		continue;
+	        	for (Failure failedCase : failedCases){
+	        		System.out.println(failedCase.getClass().toString());
+	        		System.out.println(failedCase.getDescription().getClass().toString());
+	        		Request retryRequest = Request.method(failedCase.getDescription().getTestClass(), failedCase.getDescription().getMethodName());
+	        		result = junitRunner.run(retryRequest);
+	        		if (!result.wasSuccessful()) {
+	        			OntTest.logger().setBlock();
 	        		}
+	        		System.out.println(result.wasSuccessful());
 	        	}
-	        	
-	        	if (_types.isEmpty() && (_methods.isEmpty() || _methods.contains(method.getName()))) {
-	        		if (_files.isEmpty() || _files.contains(m_file)) {
-	        			tmpmethods[i++] = method;
-		        		continue;
-	        		}
-	        	}
-	        	
-        	}
+	        }
+	        
         }
-        
-        Method[] mymethods = new Method[i];
-        for (int j = 0; j < i; j++) {
-        	if (tmpmethods[j] != null) {
-        		mymethods[j] = tmpmethods[j];
-        	}
-        	else {
-        		break;
-        	}
-        }
-        
-        Arrays.sort(mymethods, new Comparator<Method>(){
-			@Override
-			public int compare(Method m1, Method m2) {
-				if (m1.getDeclaringClass().getName().equals(m2.getDeclaringClass().getName())) {
-					return m1.getName().split("_")[2].toString().compareTo(m2.getName().split("_")[2].toString());
-				} else {
-					return m1.getDeclaringClass().getName().compareTo(m2.getDeclaringClass().getName());
-				}
-			}
-        });
-        
-        for (Method m : mymethods) {
-        	System.out.println(m.getDeclaringClass().getName());
-        	System.out.println(m.getName().split("_")[2].toString());
-        }
-                
-        // Sleep(200000); 
-        long startTime = System.currentTimeMillis();
-        long endtime = startTime;
-        long costtime = 0;
-        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-        String lastClass = "";
-        for (int j = 0; j < i; j++)  {
-        	try {
-	        	Method method = mymethods[j];
-	        	if (j == 0 || !lastClass.equals(method.getDeclaringClass().getName())) {
-	        		startTime = System.currentTimeMillis();
-	        	}
-	        	
-	            // if (!method.equals(null) && method.isAnnotationPresent(org.junit.Test.class)) 
-	            if (true) {
-	                // Request request = Request.method(method.getDeclaringClass(), method.getName());
-	            	Request request = Request.aClass(all_class.get(0));
-	            	request.filterWith(new MethodNameFilter(_methods,_excludes,_types, _files));
-	                System.out.println(method.getName());
-	                Result result = junitRunner.run(request);
-	                System.out.println(result.wasSuccessful());
-	            }
-	
-	        	lastClass = method.getDeclaringClass().getName();
-	            endtime = System.currentTimeMillis();
-	            costtime = endtime - startTime;
-	            String reportstr ="[time]" + "\n" +
-						"start=" + dateformat.format(startTime) + "\n" +
-						"end=" + dateformat.format(endtime)  + "\n" +
-						"cost=" + (costtime / 1000);
-	            String repotepath = OntTest.logger().logfile().getParentFile().getAbsolutePath() + "/report.ini";
-				FileWriter reportFileWriter = new FileWriter(repotepath, false);
-				reportFileWriter.write(reportstr);
-				reportFileWriter.close();
-        	}
-        	catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }*/
     }
 	
 }
